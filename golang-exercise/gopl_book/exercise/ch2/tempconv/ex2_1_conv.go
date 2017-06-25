f97a5a4bdc8e2116972371bf4378dc127c992257
Seiya Muramatsu <muramatsu_seiya@cyberagent.co.jp>
2017-06-25 19:18:25 +0900

    ex2_1

---
+++ golang-exercise/gopl_book/exercise/ch2/tempconv/ex2_1_conv.go
@@ -0,0 +1,18 @@
+package tempconv
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+
+// Kelvin
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c - AbsoluteZeroC)
+}
+
+func KToC(k Kelvin) Celsius {
+	return Celsius(k) + AbsoluteZeroC
+}
