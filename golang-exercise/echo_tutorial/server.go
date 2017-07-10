1dbcbc583659c498b3ee825b8f6304ff0d3e930e
Seiya Muramatsu <muramatsu_seiya@cyberagent.co.jp>
2017-07-10 23:58:05 +0900

    update server.go

--- golang-exercise/echo_tutorial/server.go
+++ golang-exercise/echo_tutorial/server.go
@@ -6,10 +6,27 @@ import (
 	"github.com/labstack/echo"
 )
 
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+
+	e.GET("/", getRoot)
+	e.POST("/users", postUser)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+func getRoot(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func postUser(c echo.Context) (err error) {
+	u := new(User)
+	if err = c.Bind(u); err != nil {
+		return
+	}
+	return c.JSON(http.StatusOK, u)
 }
