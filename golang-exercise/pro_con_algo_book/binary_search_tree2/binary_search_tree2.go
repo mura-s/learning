034bf087246c640591e6f578ec7570ae1528f9fa
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2017-12-29 17:35:38 +0900

    add binary_search_tree2

---
+++ golang-exercise/pro_con_algo_book/binary_search_tree2/binary_search_tree2.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// Node node
+type Node struct {
+	Value  int
+	Parent *Node
+	Left   *Node
+	Right  *Node
+}
+
+func next(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+	n, _ := strconv.Atoi(next(sc))
+	var root *Node
+
+	for i := 0; i < n; i++ {
+		op := next(sc)
+		switch op {
+		case "insert":
+			z, _ := strconv.Atoi(next(sc))
+			if root == nil {
+				root = &Node{Value: z}
+			} else {
+				insert(root, z)
+			}
+		case "find":
+			k, _ := strconv.Atoi(next(sc))
+			if find(root, k) {
+				fmt.Println("yes")
+			} else {
+				fmt.Println("no")
+			}
+		case "print":
+			print(root)
+		}
+	}
+}
+
+func insert(root *Node, z int) {
+	var y *Node
+	x := root
+
+	for x != nil {
+		y = x
+		if z < x.Value {
+			x = x.Left
+		} else {
+			x = x.Right
+		}
+	}
+
+	node := &Node{Value: z, Parent: y}
+	if node.Value < y.Value {
+		y.Left = node
+	} else {
+		y.Right = node
+	}
+}
+
+func print(root *Node) {
+	inorder(root)
+	fmt.Println("")
+	preorder(root)
+	fmt.Println("")
+}
+
+func inorder(node *Node) {
+	if node == nil {
+		return
+	}
+	inorder(node.Left)
+	fmt.Printf(" %d", node.Value)
+	inorder(node.Right)
+}
+
+func preorder(node *Node) {
+	if node == nil {
+		return
+	}
+	fmt.Printf(" %d", node.Value)
+	preorder(node.Left)
+	preorder(node.Right)
+}
+
+func find(node *Node, k int) bool {
+	for node != nil {
+		if node.Value == k {
+			return true
+		}
+		if node.Value < k {
+			node = node.Right
+		} else {
+			node = node.Left
+		}
+	}
+	return false
+}
