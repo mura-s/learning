f146b8b1cafb1218d93215c85865a17727d6e3f8
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-03-03 23:43:35 +0900

    ディレクトリ構造を修正

--- sicp/ch1/ex1_4.scm
+++
@@ -1,5 +0,0 @@
-(define (a-plus-abs-b a b)
-  ((if (> b 0) + -) a b))
-
-(print (a-plus-abs-b 10 5))
-(print (a-plus-abs-b 10 -5))
--- sicp/sample.scm
+++
@@ -1,9 +0,0 @@
-(use compat.sicp)
-
-(print true)
-
-(define (print2 x y)
-  (print x)
-  (print y))
-
-(print2 "abc" "def")
