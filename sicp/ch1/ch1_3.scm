e186f91f4541dbdeabf02f80d13e606f94744d85
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-03-03 23:25:45 +0900

    add 1.3.3 half-interval-method

--- sicp/ch1/ch1_3.scm
+++ sicp/ch1/ch1_3.scm
@@ -55,4 +55,32 @@
            (* y b)
            (* a b))))
 
-(print (f 2 3))
+;; (print (f 2 3))
+
+;; 1.3.3
+(define (close-enough? x y)
+    (< (abs (- x y)) 0.001))
+
+(define (average a b)
+    (/ (+ a b) 2))
+
+(define (search f neg-point pos-point)
+    (let ((midpoint (average neg-point pos-point)))
+        (if (close-enough? neg-point pos-point)
+            midpoint
+            (let ((test-value (f midpoint)))
+                (cond ((positive? test-value) (search f neg-point midpoint))
+                      ((negative? test-value) (search f midpoint pos-point))
+                      (else midpoint))))))
+
+(define (half-interval-method f a b)
+    (let ((a-value (f a))
+          (b-value (f b)))
+        (cond ((and (negative? a-value) (positive? b-value)) (search f a b))
+              ((and (negative? b-value) (positive? a-value)) (search f b a))
+              (else (error "Values are not of opposite sign" a b)))))
+
+;; (print (half-interval-method sin 2.0 4.0))
+;; (print (half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+;;                              1.0
+;;                              2.0))
