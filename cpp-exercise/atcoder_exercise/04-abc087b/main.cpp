bb602cf8292e3af2a6d352a9fddf7b0273548047
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2018-11-15 15:32:50 +0900

    Add clang-format

--- cpp-exercise/atcoder_exercise/04-abc087b/main.cpp
+++ cpp-exercise/atcoder_exercise/04-abc087b/main.cpp
@@ -6,24 +6,20 @@ int B;
 int C;
 int X;
 
-int main()
-{
-    cin >> A >> B >> C >> X;
-    int count = 0;
+int main() {
+  cin >> A >> B >> C >> X;
+  int count = 0;
 
-    for (int i = 0; i <= A; i++)
-    {
-        for (int j = 0; j <= B; j++)
-        {
-            for (int k = 0; k <= C; k++)
-            {
-                int sum = 500 * i + 100 * j + 50 * k;
-                if (sum == X)
-                    count++;
-            }
-        }
+  for (int i = 0; i <= A; i++) {
+    for (int j = 0; j <= B; j++) {
+      for (int k = 0; k <= C; k++) {
+        int sum = 500 * i + 100 * j + 50 * k;
+        if (sum == X)
+          count++;
+      }
     }
+  }
 
-    cout << count << endl;
-    return 0;
+  cout << count << endl;
+  return 0;
 }
