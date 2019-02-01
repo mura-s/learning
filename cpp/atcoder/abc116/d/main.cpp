37bcc6230e5115b4c6581b13e864817bc5b65076
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2019-02-01 22:16:55 +0900

    Update abc116/d

--- cpp/atcoder/abc116/d/main.cpp
+++ cpp/atcoder/abc116/d/main.cpp
@@ -1,77 +1,55 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <string>
-#include <unordered_set>
-#include <utility>
 #include <vector>
 using namespace std;
 
 typedef long long ll;
 
-long point(vector<pair<int, long>> &vec) {
-  unordered_set<int> set;
-  long point = 0;
-  for (auto &p : vec) {
-    set.insert(p.first);
-    point += p.second;
-  }
-  long size = set.size();
-  return point + size * size;
-}
-
 int main() {
   int N, K;
   cin >> N >> K;
-  vector<pair<int, long>> vec;
+  map<int, vector<int>> mp;
   for (int i = 0; i < N; i++) {
     int t, d;
     cin >> t >> d;
-    vec.push_back(make_pair(t, d));
+    mp[t].push_back(d);
   }
 
-  sort(vec.begin(), vec.end(),
-       [](const pair<int, long> &p1, pair<int, long> &p2) {
-         return p1.second > p2.second;
-       });
-
-  vector<pair<int, long>> resultVec;
-  for (int i = 0; i < K; i++) {
-    resultVec.push_back(make_pair(vec[i].first, vec[i].second));
-  }
-  long result = point(resultVec);
-
-  int m[100001] = {};
-  for (int i = K - 1; i >= 0; i--) {
-    int type = vec[i].first;
-    if (m[type] == 0) {
-      m[type] = i;
-      continue;
-    }
-
-    bool found = false;
-    int cand = m[type];
-
-    for (int j = K; j < N; j++) {
-      int type2 = vec[j].first;
-      if (type != type2) {
-        pair<int, long> tmp = resultVec[cand];
-        resultVec[cand] = vec[j];
-        vec[j] = tmp;
-        m[type2] = cand;
-        found = true;
-        break;
-      }
-    }
-
-    if (found) {
-      m[type] = i;
-      long value = point(resultVec);
-      if (value > result) {
-        result = value;
-      }
+  // split into max values of each type and others
+  vector<int> maxes, others;
+  for (auto &p : mp) {
+    vector<int> v = p.second;
+    sort(v.rbegin(), v.rend());
+    maxes.push_back(v[0]);
+    for (int i = 1; i < v.size(); i++) {
+      others.push_back(v[i]);
     }
   }
 
-  cout << result << endl;
+  // reverse sort
+  sort(maxes.rbegin(), maxes.rend());
+  sort(others.rbegin(), others.rend());
+
+  // cumulative sum
+  vector<ll> maxes_sum(N + 1, 0);
+  for (int i = 0; i < maxes.size(); i++) {
+    maxes_sum[i + 1] = maxes_sum[i] + maxes[i];
+  }
+  vector<ll> others_sum(N + 1, 0);
+  for (int i = 0; i < others.size(); i++) {
+    others_sum[i + 1] = others_sum[i] + others[i];
+  }
+
+  // find best k
+  int kmax = min(K, (int)maxes.size());
+  ll ans = 0;
+  for (ll k = 1; k <= kmax; k++) {
+    ll sum = maxes_sum[k] + others_sum[K - k] + k * k;
+    ans = max(ans, sum);
+  }
+  cout << ans << endl;
+
   return 0;
 }
