2287decb514ac5cc12221f950adf5e7151e49ad7
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2019-02-07 11:43:12 +0900

    Update work

--- cpp/atcoder/work/main.cpp
+++ cpp/atcoder/work/main.cpp
@@ -1,10 +1,20 @@
 #include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iomanip>
 #include <iostream>
+#include <map>
+#include <queue>
+#include <set>
 #include <string>
 #include <vector>
 using namespace std;
 
 int main() {
-  cout << "" << endl;
+  int N = 0;
+
+  cout << N << "\n";
   return 0;
 }
