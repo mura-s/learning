be74bacd1ddc1d04829f4837c02f83710b8ad28d
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2019-02-19 14:59:30 +0900

    Add abc103/d

--- cpp/atcoder/abc103/d/main.cpp
+++ cpp/atcoder/abc103/d/main.cpp
@@ -12,12 +12,31 @@
 #include <vector>
 using namespace std;
 
+bool cmp(const pair<int, int> &a, const pair<int, int> &b) {
+  return a.second < b.second;
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  int N;
-  cin >> N;
+  int N, M;
+  cin >> N >> M;
+  vector<pair<int, int>> ab;
+  for (int i = 0; i < M; ++i) {
+    int ai, bi;
+    cin >> ai >> bi;
+    ab.push_back(make_pair(ai, bi));
+  }
+  sort(ab.begin(), ab.end(), cmp);
 
-  cout << N << endl;
+  int ans = 0, end = 0;
+  for (int i = 0; i < M; ++i) {
+    if (end <= ab[i].first) {
+      end = ab[i].second;
+      ++ans;
+    }
+  }
+
+  cout << ans << endl;
   return 0;
 }
