bbfbdaa6f20af1d619af1bcd9aba74efb536d2d7
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2019-02-20 22:44:20 +0900

    Add ants_book/2-1-1 template

---
+++ cpp/atcoder/ants_book/ch2/2-1-1_abc045_c/main.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int N;
+  cin >> N;
+
+  cout << N << endl;
+  return 0;
+}
