01ef944cc5413d0a9589cdf9da15321f74712b45
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2019-03-23 00:33:20 +0900

    add 2-5-5

---
+++ cpp/procon/ants_book/ch2/2-5-5_abc079_d/test.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+all_ac=true
+
+try() {
+    input="$1"
+    expected="$2"
+
+    actual="$(echo $input | ./main)"
+
+    if [ "$actual" != "$expected" ]; then
+        echo "WA"
+        echo "expected:"
+        echo "$expected"
+        echo
+        echo "actual:"
+        echo "$actual"
+        echo
+
+        all_ac=false
+        return
+    fi
+    echo "AC"
+    echo
+}
+
+# test cases
+echo "[Sample1]"
+input1="$(cat << EOS
+2 4
+0 9 9 9 9 9 9 9 9 9
+9 0 9 9 9 9 9 9 9 9
+9 9 0 9 9 9 9 9 9 9
+9 9 9 0 9 9 9 9 9 9
+9 9 9 9 0 9 9 9 9 2
+9 9 9 9 9 0 9 9 9 9
+9 9 9 9 9 9 0 9 9 9
+9 9 9 9 9 9 9 0 9 9
+9 9 9 9 2 9 9 9 0 9
+9 2 9 9 9 9 9 9 9 0
+-1 -1 -1 -1
+8 1 1 8
+EOS
+)"
+expected1="$(cat << EOS
+12
+EOS
+)"
+try "$input1" "$expected1"
+
+echo "[Sample2]"
+input2="$(cat << EOS
+5 5
+0 999 999 999 999 999 999 999 999 999
+999 0 999 999 999 999 999 999 999 999
+999 999 0 999 999 999 999 999 999 999
+999 999 999 0 999 999 999 999 999 999
+999 999 999 999 0 999 999 999 999 999
+999 999 999 999 999 0 999 999 999 999
+999 999 999 999 999 999 0 999 999 999
+999 999 999 999 999 999 999 0 999 999
+999 999 999 999 999 999 999 999 0 999
+999 999 999 999 999 999 999 999 999 0
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+EOS
+)"
+expected2="$(cat << EOS
+0
+EOS
+)"
+try "$input2" "$expected2"
+
+echo "[Sample3]"
+input3="$(cat << EOS
+3 5
+0 4 3 6 2 7 2 5 3 3
+4 0 5 3 7 5 3 7 2 7
+5 7 0 7 2 9 3 2 9 1
+3 6 2 0 2 4 6 4 2 3
+3 5 7 4 0 6 9 7 6 7
+9 8 5 2 2 0 4 7 6 5
+5 4 6 3 2 3 0 5 4 3
+3 6 2 3 4 2 4 0 8 9
+4 6 5 4 3 5 3 2 0 8
+2 1 3 4 5 7 8 6 4 0
+3 5 2 6 1
+2 5 3 2 1
+6 9 2 5 6
+EOS
+)"
+expected3="$(cat << EOS
+47
+EOS
+)"
+try "$input3" "$expected3"
+
+echo "[Result]"
+if ! $all_ac; then
+    echo "WA"
+    echo
+    exit 1
+fi
+echo "All AC"
