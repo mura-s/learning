7d5407803afaa8782a595340b9cadebe9f4d8182
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2019-04-28 09:54:58 +0900

    update abc125/d

--- cpp/procon/atcoder/abc125/d/main.cpp
+++ cpp/procon/atcoder/abc125/d/main.cpp
@@ -16,72 +16,39 @@ typedef long long ll;
 
 ll N;
 vector<ll> A;
-bool zero = false;
-int zero_idx = 0;
+vector<ll> abs_A;
+bool has_zero = false;
+int num_minus = 0;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   cin >> N;
   A.resize(N);
+  abs_A.resize(N);
   for (int i = 0; i < N; i++) {
     cin >> A[i];
+    abs_A[i] = abs(A[i]);
     if (A[i] == 0) {
-      zero = true;
-      zero_idx = i;
+      has_zero = true;
+    }
+    if (A[i] < 0) {
+      num_minus++;
     }
   }
 
   ll ans = 0;
-  if (zero) {
-    for (int i = 0; i < zero_idx; i++) {
-      ans += abs(A[i]);
-    }
-    if (zero_idx != N) {
-      int num_minus = 0;
-      for (int i = zero_idx; i < N; i++) {
-        if (A[i] < 0) {
-          num_minus++;
-        }
-      }
-
-      vector<ll> abs_A;
-      for (int i = zero_idx; i < N; i++) {
-        abs_A.push_back(abs(A[i]));
-      }
-      if (num_minus % 2 == 0) {
-        for (int i = 0; i < abs_A.size(); i++) {
-          ans += abs_A[i];
-        }
-      } else {
-        sort(abs_A.begin(), abs_A.end());
-        ans -= abs_A[0];
-        for (int i = 1; i < abs_A.size(); i++) {
-          ans += abs_A[i];
-        }
-      }
+  if (has_zero) {
+    for (auto &e : abs_A) {
+      ans += e;
     }
   } else {
-    int num_minus = 0;
-    for (int i = 0; i < N; i++) {
-      if (A[i] < 0) {
-        num_minus++;
-      }
+    for (auto &e : abs_A) {
+      ans += e;
     }
-    vector<ll> abs_A(N);
-    for (int i = 0; i < N; i++) {
-      abs_A[i] = abs(A[i]);
-    }
-    if (num_minus % 2 == 0) {
-      for (int i = 0; i < N; i++) {
-        ans += abs_A[i];
-      }
-    } else {
+    if (num_minus % 2 != 0) {
       sort(abs_A.begin(), abs_A.end());
-      ans -= abs_A[0];
-      for (int i = 1; i < N; i++) {
-        ans += abs_A[i];
-      }
+      ans -= (2 * abs_A[0]);
     }
   }
 
