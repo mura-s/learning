893546f590db9f74d7db914280e2f734cdbde0be
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2019-05-04 17:22:04 +0900

    update

--- cpp/procon/atcoder/abc111/d/main.cpp
+++ cpp/procon/atcoder/abc111/d/main.cpp
@@ -1,80 +1,29 @@
 #include <algorithm>
-#include <cassert>
 #include <climits>
 #include <cmath>
-#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <map>
 #include <queue>
 #include <set>
+#include <stack>
 #include <string>
 #include <vector>
 using namespace std;
+typedef long long ll;
+// const ll MOD = 1000000007;
+// const int INF = 2000000000;
 
-// 部分点解法
+int N;
+
+// TODO:
 int main() {
-  cin.tie(0);
   ios::sync_with_stdio(false);
-  int N;
+  cin.tie(0);
   cin >> N;
-  vector<long> xs(N + 1), ys(N + 1); // 1-indexed
-  for (int i = 1; i <= N; ++i) {
-    long xi, yi;
-    cin >> xi >> yi;
-    xs[i] = xi;
-    ys[i] = yi;
-  }
 
-  bool ok = true;
-  long maxd = abs(xs[1]) + abs(ys[1]);
-  int parity = (abs(xs[1]) + abs(ys[1])) % 2;
-  for (int i = 2; i <= N; ++i) {
-    int parity2 = (abs(xs[i]) + abs(ys[i])) % 2;
-    if (parity != parity2) {
-      ok = false;
-    }
-    maxd = max(maxd, abs(xs[i]) + abs(ys[i]));
-  }
+  int ans = 0;
 
-  if (!ok) {
-    cout << -1 << endl;
-    return 0;
-  }
-  if (maxd > 40) {
-    cout << -1 << endl;
-    return 0;
-  }
-
-  cout << maxd << endl;
-  for (int i = 1; i <= maxd; ++i) {
-    cout << 1 << " ";
-  }
-  cout << endl;
-
-  for (int i = 1; i <= N; ++i) {
-    for (int j = 0; j < abs(xs[i]); ++j) {
-      if (xs[i] > 0) {
-        cout << "R";
-      } else {
-        cout << "L";
-      }
-    }
-
-    for (int j = 0; j < abs(ys[i]); ++j) {
-      if (ys[i] > 0) {
-        cout << "U";
-      } else {
-        cout << "D";
-      }
-    }
-
-    for (int j = 0; j + abs(xs[i]) + abs(ys[i]) < maxd; j += 2) {
-      cout << "LR";
-    }
-
-    cout << endl;
-  }
-
+  cout << ans << endl;
   return 0;
 }
--- cpp/procon/atcoder/abc111/d/test.sh
+++ cpp/procon/atcoder/abc111/d/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+all_ac=true
+
 try() {
     input="$1"
     expected="$2"
@@ -6,15 +8,23 @@ try() {
     actual="$(echo $input | ./main)"
 
     if [ "$actual" != "$expected" ]; then
-        echo "$expected expected, but got $actual"
-        exit 1
+        echo "WA"
+        echo "expected:"
+        echo "$expected"
+        echo
+        echo "actual:"
+        echo "$actual"
+        echo
+
+        all_ac=false
+        return
     fi
+    echo "AC"
+    echo
 }
 
 # test cases
-echo "start"
-
-# sample1
+echo "[Sample1]"
 input1="$(cat << EOS
 3
 -1 0
@@ -28,12 +38,12 @@ expected1="$(cat << EOS
 RL
 UU
 DR
+
 EOS
 )"
 try "$input1" "$expected1"
-echo "sample1 ok"
 
-# sample2
+echo "[Sample2]"
 input2="$(cat << EOS
 5
 0 0
@@ -48,9 +58,8 @@ expected2="$(cat << EOS
 EOS
 )"
 try "$input2" "$expected2"
-echo "sample2 ok"
 
-# sample3
+echo "[Sample3]"
 input3="$(cat << EOS
 2
 1 1
@@ -62,12 +71,12 @@ expected3="$(cat << EOS
 1 1
 RU
 UR
+
 EOS
 )"
 try "$input3" "$expected3"
-echo "sample3 ok"
 
-# sample4
+echo "[Sample4]"
 input4="$(cat << EOS
 3
 -7 -3
@@ -84,6 +93,11 @@ DULRD
 EOS
 )"
 try "$input4" "$expected4"
-echo "sample4 ok"
 
-echo "ok"
+echo "[Result]"
+if ! $all_ac; then
+    echo "WA"
+    echo
+    exit 1
+fi
+echo "All AC"
