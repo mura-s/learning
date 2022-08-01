b0d304b580f04be306ef46b718b53bf8c0da7bb9
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2022-08-02 00:50:40 +0900

    update

--- rust/hello-rust/src/main.rs
+++ rust/hello-rust/src/main.rs
@@ -1,5 +1,24 @@
+use std::env;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("Gcd of {:?} is {}", numbers, d);
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
