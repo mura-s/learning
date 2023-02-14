eaf275a57829d1d75ca040e1e43be40a088f7f88
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2023-02-14 23:38:03 +0900

    add task list app

---
+++ java/tasklist/src/test/java/jp/muras/projava/tasklist/TaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.muras.projava.tasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
