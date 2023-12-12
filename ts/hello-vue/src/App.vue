560576086a17bafa052cd44966589502e9e596f4
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2023-12-12 21:55:46 +0900

    update

--- ts/hello-vue/src/App.vue
+++ ts/hello-vue/src/App.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import OneInfo from './components/OneInfo.vue'
 
-const name = ref('Test Name')
+const propsTitle = ref('発生した乱数')
+const rand = Math.round(Math.random() * 100)
+const propsContent = ref(rand)
+
+setTimeout(() => {
+  propsTitle.value = '発生した乱数(変更後)'
+}, 1000)
 </script>
 
 <template>
-  <h1>Hello, {{ name }}!</h1>
+  <h1>Props基礎</h1>
+  <section>
+    <h2>テンプレート変数を利用</h2>
+    <OneInfo v-bind:title="propsTitle" v-bind:content="propsContent" />
+  </section>
 </template>
---
+++ ts/hello-vue/src/components/OneInfo.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+  content: number
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <section class="box">
+    <h4>{{ title }}</h4>
+    <p>{{ content }}</p>
+  </section>
+</template>
+
+<style scoped>
+.box {
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
