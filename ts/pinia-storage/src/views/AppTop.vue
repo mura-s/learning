bfac00a13349c30f4aabc6ccd85890ab103b9b41
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2023-12-13 19:11:21 +0900

    update

---
+++ ts/pinia-storage/src/views/AppTop.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+
+console.log("AppTop.vue");
+</script>
+
+<template>
+  <h1>TOP</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li>TOP</li>
+    </ul>
+  </nav>
+  <section>
+    <p>
+      <RouterLink v-bind:to="{ name: 'MemberList' }">
+        会員管理はこちら
+      </RouterLink>
+    </p>
+  </section>
+</template>
