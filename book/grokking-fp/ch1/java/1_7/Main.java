2d4da2336014dc6902f7ce1a65604b83e18cd577
Seiya Muramatsu <4702673+mura-s@users.noreply.github.com>
2024-04-28 11:00:24 +0900

    update

--- book/grokking-fp/ch1/java/1_7/Main.java
+++ book/grokking-fp/ch1/java/1_7/Main.java
@@ -1,5 +1,23 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println(calculateScore("imperative"));
+        System.out.println(calculateScore("no"));
+        System.out.println(wordScore("declarative"));
+        System.out.println(wordScore("yes"));
+    }
+
+    public static int calculateScore(String word) {
+        int score = 0;
+        for (char c : word.toCharArray()) {
+            if (c != 'a') {
+                score++;
+            }
+        }
+        return score;
+    }
+
+    public static int wordScore(String word) {
+        return (int) word.chars().filter(c -> c != 'a').count();
+        // return word.replace("a", "").length();
     }
 }
